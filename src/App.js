982e0134c311a1079f9a8535299f109dd02fdffe
Brian Meehan <bmeehan1020@gmail.com>
2021-02-05 09:43:03 -0500

    Add user input form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
@@ -6,7 +8,22 @@ function App() {
       <header className="App-header">
         <h1>WIT Remote Background Generator</h1>
       </header>
-
+      <div className="App-body">
+        <Form>
+          <Form.Group as={Row} controlId="formGroupFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group as={Row} controlId="formGroupLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group as={Row} controlId="formGroupPronouns">
+            <Form.Label>Pronouns (optional) </Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
